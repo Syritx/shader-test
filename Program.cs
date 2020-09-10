a97aa82c247177f927b327986d209d73f70f36e3
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-09-10 14:13:48 -0400

    added full source

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace shaders
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Game(1000,720);
+        }
+    }
+
+    class Game : GameWindow
+    {
+        int VertexBufferObject;
+        int VertexArrayObject;
+
+        float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+             0.5f, -0.5f, 0.0f, //Bottom-right vertex
+             0.0f,  0.5f, 0.0f  //Top vertex
+        };
+
+        Triangle triangle;
+
+        string vertexShaderSource;
+        string fragmentShaderSource;
+        string main_path = Directory.GetCurrentDirectory();
+
+        public Game(int width, int height) : base(width,height,GraphicsMode.Default,"SHADER TEST")
+        {
+            fragmentShaderSource = getShaderCode("FragmentShader.glsl");
+            vertexShaderSource = getShaderCode("VertexShader.glsl");
+
+            Console.WriteLine(fragmentShaderSource);
+            start();
+        }
+
+        void start() {
+            Run(60);
+            triangle = new Triangle(vertexShaderSource,fragmentShaderSource);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            VertexArrayObject = GL.GenVertexArray();
+
+            GL.VertexAttribPointer(0,3,VertexAttribPointerType.Float,false,3 * sizeof(float),0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindVertexArray(VertexArrayObject);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            try { 
+                triangle.render();
+            } catch(Exception e1) {}
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0, 0, 0, 0);
+            VertexBufferObject = GL.GenBuffer();
+            base.OnLoad(e);
+        }
+
+        protected override void OnUnload(EventArgs e) { 
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VertexBufferObject);
+
+            triangle.Dispose();
+            base.OnUnload(e);
+        }
+
+
+        string getShaderCode(string fileName)
+        {
+            // FILES MUST BE STORED IN THE bin/Debug FOLDER
+
+            string code = "";
+            using (StreamReader reader = new StreamReader(main_path+"/"+fileName, Encoding.UTF8)) {
+                code = reader.ReadToEnd();
+            }
+
+            return code;
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL;
+
+namespace shaders
+{
+    public class Triangle
+    {
+        int handle;
+        int vertexShader, fragmentShader;
+
+        public Triangle(string vertexShaderSource, string fragmentShaderSource)
+        {
+            vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, vertexShaderSource);
+
+            fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, fragmentShaderSource);
+
+            GL.CompileShader(vertexShader);
+            GL.CompileShader(fragmentShader);
+
+            handle = GL.CreateProgram();
+
+            GL.AttachShader(handle, vertexShader);
+            GL.AttachShader(handle, fragmentShader);
+
+            GL.LinkProgram(handle);
+
+            GL.DetachShader(handle, vertexShader);
+            GL.DetachShader(handle, fragmentShader);
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+        }
+
+        public void render() {
+            GL.UseProgram(handle);
+        }
+
+        bool disposed = false;
+        protected virtual void Dispose(bool disposing) {
+
+            if (!disposed) {
+                GL.DeleteProgram(handle);
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Triangle() {
+            GL.DeleteProgram(handle);
+        }
+    }
+}
